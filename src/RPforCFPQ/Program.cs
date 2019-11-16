c29f72f24649701df01a344592de81f3068da635
Vladimir Korolikhin <volodyk.99@mail.ru>
2019-11-16 12:02:09 +0300

    some changes

---
+++ src/RPforCFPQ/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using No.Comparers;
+using Parser;
+using Parser.Grammars;
+using Parser.Graphs;
+using Parser.Relational;
+using Parser.Utilities;
+using System.Collections.Generic;
+
+namespace RPforCFPQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IGraphEngineFactory engineFactory = new DictionaryGraphEngineFactory();
+            IAtomEngineFactory atomEngineFactory = new DagAtomEngineFactory(engineFactory);
+            
+            DictionaryGraphEngineFactory dictionaryGraphEngineFactory = new DictionaryGraphEngineFactory();
+            CommutativeWrapEngineFactory commutativeWrapEngineFactory =new CommutativeWrapEngineFactory(null);
+            IRelationEngineFactory relationEngineFactory =
+                new DagRelationEngineFactory(commutativeWrapEngineFactory, dictionaryGraphEngineFactory);
+            IRelationOperantFactory relationOperantFactory = new RelationOperantFactory(relationEngineFactory);
+
+            AtomicOptimization[] atomicOptimizations = new AtomicOptimization[0];
+            
+            ParserGenerator parserGenerator = 
+                new RelationalParserGenerator(atomEngineFactory, 
+                    relationOperantFactory, false, atomicOptimizations);
+
+            List<Char[]> inputs = new List<char[]>();
+            inputs.Add(new []{'(', '(', ')', '(', ')', ')'});
+            
+            IValuationSemiring<TValue>
+            parserGenerator.CreateParser(SampleGrammar.Parentheses, )
+        }
+    }
+} 
